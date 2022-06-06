efe01a9c950ead5ae3d6bd24d0b30c3604013390
Kuba <bagro2803@gmail.com>
2022-06-06 13:42:32 +0200

    Version 2.0, add DispatcherTimer in Player vs AI, change GUI

---
+++ Players.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TicTacToe_ver_3._0
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Players.xaml
+    /// </summary>
+    public partial class Players : Window
+    {
+        public Players()
+        {
+            InitializeComponent();
+        }
+
+        private void SavePlayers_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindow = new MainWindow();
+            if(txtBoxPlayer1.Text == string.Empty && txtBoxPlayer2.Text == string.Empty)
+            {
+                mainWindow.Player1.Content = "Player 1";
+                mainWindow.Player2.Content = "Player 2";
+                this.Close();
+
+            }
+            else
+            {
+                mainWindow.Player1.Content = txtBoxPlayer1.Text;
+                mainWindow.Player2.Content = txtBoxPlayer2.Text;
+                this.Close();
+
+            }
+       
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TicTacToe_ver_3._0
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start(Application.ResourceAssembly.Location);
+            Application.Current.Shutdown();
+
+        }
+
+        private void CloseApp_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void PlayAgain_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
